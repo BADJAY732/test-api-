433e167d350cdbb6ae19df39a3b0cdfc55b23f1b
BADJAY732 <aomzaa732@gmail.com>
2025-12-25 14:31:35 +0700

    first commit test Interview

---
+++ database/expense.sql
@@ -0,0 +1,23 @@
+-- ===============================
+-- ใช้ฐานข้อมูล
+-- ===============================
+CREATE DATABASE IF NOT EXISTS user_api;
+USE user_api;
+
+DROP TABLE IF EXISTS expenses;
+
+-- ===============================
+-- สร้างตาราง expenses
+-- ===============================
+CREATE TABLE expenses (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    expense_date DATE NOT NULL,
+    amount DECIMAL(10,2) NOT NULL
+);
+
+INSERT INTO expenses (expense_date, amount) VALUES
+('2024-10-05', 1200),
+('2024-10-20', 2000),
+('2024-11-10', 1800),
+('2024-12-01', 2500),
+('2025-01-15', 4000);
---
+++ index.js
@@ -0,0 +1,63 @@
+console.log("SERVER STARTED");
+
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+// ✅ mock data
+const users = [
+  { id: 1, name: "Jay", phone: "0990190339" },
+  { id: 2, name: "John", phone: "0812345678" },
+  { id: 3, name: "Jane", phone: "0899999999" },
+  { id: 4, name: "James", phone: "0822222222" },
+  { id: 5, name: "Jenny", phone: "0833333333" }
+];
+
+app.get("/users", (req, res) => {
+  const search = req.query.search || "";
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 2;
+
+  // ✅ validate
+  if (page < 1) {
+    return res.status(400).json({ message: "page must be >= 1" });
+  }
+
+  if (limit < 1) {
+    return res.status(400).json({ message: "limit must be >= 1" });
+  }
+
+  // 🔍 search
+  const filteredUsers = users.filter(u =>
+    u.name.toLowerCase().includes(search.toLowerCase()) ||
+    u.phone.includes(search)
+  );
+
+  const totalPage = Math.ceil(filteredUsers.length / limit);
+
+  // ❗ page เกิน
+  if (page > totalPage && totalPage > 0) {
+    return res.status(404).json({ message: "page not found" });
+  }
+
+  // 📄 pagination
+  const startIndex = (page - 1) * limit;
+  const paginatedUsers = filteredUsers.slice(
+    startIndex,
+    startIndex + limit
+  );
+
+  res.json({
+    page,
+    limit,
+    total: filteredUsers.length,
+    totalPage,
+    hasNext: page < totalPage,
+    data: paginatedUsers
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
